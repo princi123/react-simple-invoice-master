c27ef8233c16c68217ddadf792781a573e17abc3
princi <princi.rubydeveloper@gmail.com>
2021-02-24 09:51:12 +0530

    Second Commit

---
+++ src/components/Invoice/Invoice.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react'
+import styles from './Invoice.module.scss'
+
+import LineItems from './LineItems'
+
+import uuidv4 from 'uuid/v4'
+import {getCurrentDate} from './getCurrentDate'
+
+
+class Invoice extends Component {
+
+  locale = 'en-US'
+  currency = 'INR'
+
+  state = {
+    taxRate: 0.00,
+    lineItems: [
+      {
+        id: 'initial',      // react-beautiful-dnd unique key
+        name: '',
+        description: '',
+        quantity: 0,
+        price: 0.00,
+      },
+    ]
+  }
+
+  handleInvoiceChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleLineItemChange = (elementIndex) => (event) => {
+    let lineItems = this.state.lineItems.map((item, i) => {
+      if (elementIndex !== i) return item
+      return {...item, [event.target.name]: event.target.value}
+    })
+    this.setState({lineItems})
+  }
+
+  handleAddLineItem = (event) => {
+    this.setState({
+      // use optimistic uuid for drag drop; in a production app this could be a database id
+      lineItems: this.state.lineItems.concat(
+        [{ id: uuidv4(), name: '', description: '', quantity: 0, price: 0.00 }]
+      )
+    })
+  }
+
+  handleRemoveLineItem = (elementIndex) => (event) => {
+    this.setState({
+      lineItems: this.state.lineItems.filter((item, i) => {
+        return elementIndex !== i
+      })
+    })
+  }
+
+  handleReorderLineItems = (newLineItems) => {
+    this.setState({
+      lineItems: newLineItems,
+    })
+  }
+
+  handleFocusSelect = (event) => {
+    event.target.select()
+  }
+
+  handlePayButtonClick = () => {
+    window.print();
+  }
+
+  formatCurrency = (amount) => {
+    return (new Intl.NumberFormat(this.locale, {
+      style: 'currency',
+      currency: this.currency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).format(amount))
+  }
+
+  calcTaxAmount = (c) => {
+    return c * (this.state.taxRate / 100)
+  }
+
+  calcLineItemsTotal = () => {
+    return this.state.lineItems.reduce((prev, cur) => (prev + (cur.quantity * cur.price)), 0)
+  }
+
+  calcTaxTotal = () => {
+    return this.calcLineItemsTotal() * (this.state.taxRate / 100)
+  }
+
+  calcGrandTotal = () => {
+    return this.calcLineItemsTotal() + this.calcTaxTotal()
+  }
+
+  render = () => {
+    return (
+
+      <div className={styles.invoice}>
+        <div>
+
+          <div>
+            <div className={`${styles.valueTable} ${styles.to}`}>
+              <div className={styles.row}>
+                <div className={styles.label}>Date</div>
+                <div className={`${styles.value} ${styles.date}`}>{getCurrentDate()}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h2 className={styles.center}>NS</h2>
+
+          <LineItems
+            items={this.state.lineItems}
+            currencyFormatter={this.formatCurrency}
+            addHandler={this.handleAddLineItem}
+            changeHandler={this.handleLineItemChange}
+            focusHandler={this.handleFocusSelect}
+            deleteHandler={this.handleRemoveLineItem}
+            reorderHandler={this.handleReorderLineItems}
+          />
+
+        <div>
+          <form>
+            <div className={styles.valueTable}>
+              <div className={styles.row}>
+                <div className={styles.label}>Subtotal</div>
+                <div className={styles.princi}>{this.formatCurrency(this.calcLineItemsTotal())}</div>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div className={styles.pay}>
+          <button className={styles.payNow} onClick={this.handlePayButtonClick}>Print Now</button>
+        </div>
+
+      </div>
+
+    )
+  }
+
+}
+
+export default Invoice
---
+++ src/components/Invoice/LineItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { MdCancel as DeleteIcon } from 'react-icons/md'
+import styles from './LineItem.module.scss'
+
+
+class LineItem extends Component {
+
+  render = () => {
+
+    const { index, name, quantity, price } = this.props
+
+    return (
+      <div className={styles.lineItem}>
+        <div>{index + 1})Item</div>
+        <div><input name="price" type="number" value={price} onChange={this.props.changeHandler(index)} onFocus={this.props.focusHandler} /></div>
+        <div><input name="quantity" type="number" value={quantity} onChange={this.props.changeHandler(index)} onFocus={this.props.focusHandler} /></div>
+        <div>{ quantity * price }</div>
+        <div>
+          <button type="button"
+            className={styles.deleteItem}
+            onClick={this.props.deleteHandler(index)}
+          ><DeleteIcon size="1.25em" /></button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LineItem
+
+LineItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  name: PropTypes.string,
+  description: PropTypes.string,
+  quantity: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+}
+
+
