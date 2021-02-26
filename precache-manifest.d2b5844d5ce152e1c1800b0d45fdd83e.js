a3fb5795f9a933e2e9886a88160a452a598f9e06
princi <princi.rubydeveloper@gmail.com>
2021-02-26 12:57:01 +0530

    Updates

---
+++ precache-manifest.d2b5844d5ce152e1c1800b0d45fdd83e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6e9bc57758dae4719c6669c24a9c43",
+    "url": "/react-simple-invoice-master/index.html"
+  },
+  {
+    "revision": "90fc220f08a6e5d398f9",
+    "url": "/react-simple-invoice-master/static/css/main.e55c154b.chunk.css"
+  },
+  {
+    "revision": "2fd40e02a8a02c31dc64",
+    "url": "/react-simple-invoice-master/static/js/2.8ed97e22.chunk.js"
+  },
+  {
+    "revision": "90fc220f08a6e5d398f9",
+    "url": "/react-simple-invoice-master/static/js/main.dafffc09.chunk.js"
+  },
+  {
+    "revision": "51e4f4e4f71ab8933843",
+    "url": "/react-simple-invoice-master/static/js/runtime~main.4f2214d3.js"
+  }
+]);
